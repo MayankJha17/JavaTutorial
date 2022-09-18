259684635130ab0d6060abc91c1796a57f771897
Mayank Jha <mayankjha179@gmail.com>
2022-09-19 00:12:43 +0530

    Removed main method from threatre class

--- Collections/Threatre.java
+++ Collections/Threatre.java
@@ -89,26 +89,5 @@ public class Threatre {
         
     }
 
-
-
-
-    public static void main(String[] args) {
-        Threatre threatre = new Threatre("Empire", 6, 6);
-        List<Threatre.Seat> seatsCopy = new ArrayList<>(threatre.seats);
-        String seatNum = "E01";
-        if(threatre.getSeatByNum(seatNum)){
-               System.out.println("Please pay");
-        }
-        else{
-            System.out.println("Sorry seat is already reserved");
-        }
-        if(threatre.getSeatByNum(seatNum)){
-            System.out.println("Please pay");
-     }
-     else{
-         System.out.println("Sorry seat is already reserved");
-     }
-    }
-
 }
 
