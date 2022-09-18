7be427d8fe3210f86ef3f2d918a8c244becddfdb
Mayank Jha <mayankjha179@gmail.com>
2022-09-19 00:46:24 +0530

    Added reverse and shuffle method of collections

--- Collections/Main.java
+++ Collections/Main.java
@@ -1,6 +1,7 @@
 package Collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -8,9 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Threatre threatre = new Threatre("Empire", 6, 6);
         List<Threatre.Seat> seatsCopy = new ArrayList<>(threatre.seats);
-        for (Threatre.Seat seat : seatsCopy) {
-            System.out.println(seat.getNumSeat());
-        }
+        printList(seatsCopy);
         System.out.println("=========================================");
 
         seatsCopy.get(1).reserve(); // reserving seat A01 which means seatsCopy and seats(list inside class) are pointing same
@@ -28,6 +27,22 @@ public class Main {
      else{
          System.out.println("Sorry seat is already reserved");
      }
+     System.out.println("=========================================");
+
+      Collections.reverse(seatsCopy);  // reversing the order of the list
+      printList(seatsCopy);
+
+      Collections.shuffle(seatsCopy); // randomly shuffle the list 
+      printList(seatsCopy);
+
+    }
+
+    public static void printList(List<Threatre.Seat> seats){
+
+        for (Threatre.Seat seat : seats) {
+            System.out.print(" " + seat.getNumSeat());
+        }
+
     }
     
 }
